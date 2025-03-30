152c221a9dd6a2b221477c93856dccc96fb9b7a5
ctrindadedev <caiomedtrindade@gmail.com>
2025-03-30 18:38:14 -0300

    plurality - c/js

---
+++ Week-3-Algorithms/ProblemSet_3/Plurality/plurality/plurality.js
@@ -0,0 +1,49 @@
+
+// Max number of candidates
+const MAX = 9
+
+// Candidates have name and vote count
+canditato = {
+    nome: [],
+    votes: 0
+}
+
+
+// Array of candidates
+let i = 0
+let candidatos = [];
+
+// Number of candidates
+let candidatos_counter = 0
+
+// Function prototypes
+ vote = (nome) => {
+    return nome
+
+ }
+print_winner = () => {
+
+}
+
+ // Check for invalid usage
+
+    // Populate array of candidates
+
+
+    // Loop over all voters
+
+    // Display winner of election
+    print_winner();
+
+
+//Update vote totals given a new vote
+{
+    // TODO
+   
+}
+
+// Print the winner (or winners) of the election
+{
+    // TODO
+    
+}
