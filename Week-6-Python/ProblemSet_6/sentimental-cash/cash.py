739786d0bb68658be025d4554ab8a0fe679ba8dc
ctrindadedev <caiomedtrindade@gmail.com>
2025-05-02 09:21:39 -0300

    sentimental-cash

--- Week-6-Python/ProblemSet_6/sentimental-cash/cash.py
+++ Week-6-Python/ProblemSet_6/sentimental-cash/cash.py
@@ -1,22 +1,34 @@
 coins = 0
-change = float(input("Change: "))
-while (change != 0):
-
 # Prompt the user for change owed, in cents
+change_owed = float(input("Change: "))
+#Check if the change is a positive number
+if (change_owed <= 0):
+    print("Não terá troco.")
+
 
 # Calculate how many quarters you should give customer
+while (change_owed >= 25):
+    coins += 1
+    # Subtract the value of those quarters from cents
+    change_owed = change_owed - 25
 
-# Subtract the value of those quarters from cents
+#Calculate how many dimes you should give customer
+while (change_owed >= 10):
+    coins += 1
+    #Subtract the value of those dimes from cents
+    change_owed = change_owed - 10
 
-# #Calculate how many dimes you should give customer
-
-# ubtract the value of those dimes from cents
-
-
-# #Calculate how many nickels you should give customer
-
-# #Subtract the value of those nickels from cents
+#Calculate how many nickels you should give customer
+while (change_owed >= 5):
+    coins += 1
+    #Subtract the value of those nickels from cents
+    change_owed = change_owed - 5
 
  #Calculate how many pennies you should give customer
+while (change_owed >= 1):
+    coins += 1
+# Subtract the value of those pennies from cents
+    change_owed = change_owed - 1
 
 #Print total coins
+print(coins)
