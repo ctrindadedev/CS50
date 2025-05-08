7cfe915ccbe566eb0603cecb68e3ae8574cbbe13
ctrindadedev <caiomedtrindade@gmail.com>
2025-05-07 22:08:46 -0300

    estruturação do problema homepage

---
+++ Week-8-HTML-CSS-JavaScript/ProblemSet_8/homepage/script.js
@@ -0,0 +1,5 @@
+document.getElementById("greet").addEventListener("click", () => {
+  const message = document.getElementById("message");
+  message.textContent = "Hello, welcome to the website!";
+  message.style.color = "blue";
+});
