b1b7a506d49fb9b7ccfafe03641d6cc34ef46db3
ctrindadedev <caiomedtrindade@gmail.com>
2025-08-29 18:20:29 -0300

    add birthday and finance problems distribution code

---
+++ Week-9-Flask/ProblemSet_9/birthdays/app.py
@@ -0,0 +1,52 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+
+        return redirect("/")
+
+    else:
+
+        # TODO: Display the entries in the database on index.html
+
+        return render_template("index.html")
+
+
+Implementation Details
+Complete the implementation of a web application to let users store and keep track of birthdays.
+
+When the / route is requested via GET, your web application should display, in a table, all of the people in your database along with their birthdays.
+First, in app.py, add logic in your GET request handling to query the birthdays.db database for all birthdays. Pass all of that data to your index.html template.
+Then, in index.html, add logic to render each birthday as a row in the table. Each row should have two columns: one column for the person’s name and another column for the person’s birthday.
+When the / route is requested via POST, your web application should add a new birthday to your database and then re-render the index page.
+First, in index.html, add an HTML form. The form should let users type in a name, a birthday month, and a birthday day. Be sure the form submits to / (its “action”) with a method of post.
+Then, in app.py, add logic in your POST request handling to INSERT a new row into the birthdays table based on the data supplied by the user.
+Optionally, you may also:
+
+Add the ability to delete and/or edit birthday entries.
+Add any additional features of your choosing!
