92fa319d08885672e86f40dcd0c91ae8bfeb6ce5
ctrindadedev <caiomedtrindade@gmail.com>
2025-08-29 23:10:48 -0300

    add birthday solution

--- Week-9-Flask/ProblemSet_9/birthdays/app.py
+++ Week-9-Flask/ProblemSet_9/birthdays/app.py
@@ -28,25 +28,24 @@ def index():
 
         # TODO: Add the user's entry into the database
 
+        # Get data from form
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+
+        # Insert data into database, if data is valid
+        if name and month and day:
+            db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+
+        # Go back to homepage
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
 
-        return render_template("index.html")
+        # Query for all birthdays
+        birthdays = db.execute("SELECT * FROM birthdays")
 
-
-Implementation Details
-Complete the implementation of a web application to let users store and keep track of birthdays.
-
-When the / route is requested via GET, your web application should display, in a table, all of the people in your database along with their birthdays.
-First, in app.py, add logic in your GET request handling to query the birthdays.db database for all birthdays. Pass all of that data to your index.html template.
-Then, in index.html, add logic to render each birthday as a row in the table. Each row should have two columns: one column for the person’s name and another column for the person’s birthday.
-When the / route is requested via POST, your web application should add a new birthday to your database and then re-render the index page.
-First, in index.html, add an HTML form. The form should let users type in a name, a birthday month, and a birthday day. Be sure the form submits to / (its “action”) with a method of post.
-Then, in app.py, add logic in your POST request handling to INSERT a new row into the birthdays table based on the data supplied by the user.
-Optionally, you may also:
-
-Add the ability to delete and/or edit birthday entries.
-Add any additional features of your choosing!
+        # Render birthday list
+        return render_template("index.html", birthdays=birthdays)
